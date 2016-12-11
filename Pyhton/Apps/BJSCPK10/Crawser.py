1ae6d4158e098c6c49d8c7f6ac58b79818e28baa
maka <maka@makadeMacBook-Air.local>
2016-12-11 14:02:08 +0800

    mmm

--- Pyhton/Apps/BJSCPK10/Crawser.py
+++ Pyhton/Apps/BJSCPK10/Crawser.py
@@ -37,7 +37,7 @@ class Crawser(object):
             rows = sss['rows']
             row = rows[0]
 
-            time = row['lotteryTime'][:16]
+            shijian = row['lotteryTime'][:16]
             numbers = str(row['n1']) + ',' + str(row['n2']) + ',' + str(row['n3']) + ',' + str(row['n4']) + ',' + str(
                 row['n5']) + ',' + str(row['n6']) + ',' + str(row['n7']) + ',' + str(row['n8']) + ',' + str(
                 row['n9']) + ',' + str(row['n10'])
@@ -46,7 +46,7 @@ class Crawser(object):
             sql = "INSERT INTO {0} ({1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12}) VALUES ( \
             '{13}','{14}','{15}','{16}','{17}','{18}','{19}','{20}','{21}','{22}','{23}','{24}');".format( \
                 CF.HISTAB, CF.HISQI, CF.HISTIME, CF.HISN1, CF.HISN2, CF.HISN3, CF.HISN4, CF.HISN5,CF.HISN6, CF.HISN7, CF.HISN8, CF.HISN9, CF.HISN10, \
-                qishu, time, str(row['n1']), str(row['n2']), str(row['n3']), str(row['n4']), str(row['n5']), str(row['n6']), str(row['n7']), str(row['n8']), str(row['n9']),str(row['n10']))
+                qishu, shijian, str(row['n1']), str(row['n2']), str(row['n3']), str(row['n4']), str(row['n5']), str(row['n6']), str(row['n7']), str(row['n8']), str(row['n9']),str(row['n10']))
             DBM.maka_do_sql(sql)
 
             time.sleep(1)
