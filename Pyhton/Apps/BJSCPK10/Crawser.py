acd7072b6f25f621d1458d3850944b7d91612ddd
maka <maka@makadeMacBook-Air.local>
2016-12-13 15:27:43 +0800

    mmm

--- Pyhton/Apps/BJSCPK10/Crawser.py
+++ Pyhton/Apps/BJSCPK10/Crawser.py
@@ -41,6 +41,8 @@ class Crawser(object):
         sql = "select {0} from {1} order by {2} desc limit 1".format(CF.HISTIME, CF.HISTAB, CF.HISQI)
         result = DBM.maka_do_sql(sql)
 
+        print result
+
         if result.count == 0:
             return 1
 
