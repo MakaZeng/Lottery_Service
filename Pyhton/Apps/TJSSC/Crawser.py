b5c0d5804c74be5adf38fdf7d936c0536858a1ef
maka <maka@makadeMacBook-Air.local>
2016-12-13 15:18:42 +0800

    mmm

--- Pyhton/Apps/TJSSC/Crawser.py
+++ Pyhton/Apps/TJSSC/Crawser.py
@@ -40,6 +40,10 @@ class Crawser(object):
 
         sql = "select {0} from {1} order by {2} desc limit 1".format(CF.HISTIME, CF.HISTAB, CF.HISQI)
         result = DBM.maka_do_sql(sql)
+
+        if not result:
+            return 1
+
         lastTime = result[0][0]
         print  lastTime
         lastTimeSeconds = DU.date_to_time(lastTime)
